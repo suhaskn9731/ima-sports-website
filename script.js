4846ecec48fbc730220e91dca60aa9bbb3916d34
suhaskn9731 <suhaskn9731@gmail.com>
2026-03-14 01:05:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu(){
+
+let menu = document.getElementById("menu");
+
+if(menu.style.display === "flex"){
+
+menu.style.display = "none";
+
+}else{
+
+menu.style.display = "flex";
+
+}
+
+}

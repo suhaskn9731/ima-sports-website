5f8dbfce25a3a230fa81e39c640172bc75e47fc5
suhaskn9731 <suhaskn9731@gmail.com>
2026-03-15 22:14:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,23 +61,3 @@ requestAnimationFrame(draw);
 }
 
 draw();
-
-
-
-/* CARD ANIMATION */
-
-const cards=document.querySelectorAll(".card");
-
-window.addEventListener("scroll",()=>{
-
-cards.forEach(card=>{
-
-const top=card.getBoundingClientRect().top;
-
-if(top<window.innerHeight-100){
-card.classList.add("show");
-}
-
-});
-
-});
